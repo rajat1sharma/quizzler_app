29dbb6505c46299b93f0cdcab60bbf22a019c6f3
Rajat Sharma <rajat.sharma4211@gmail.com>
2020-08-24 21:53:45 +0530

    encapsulated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+Quizbrain quizz = Quizbrain();
 
 void main() => runApp(Quizzler());
 
@@ -27,12 +30,14 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   @override
   List<Icon> scorekeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-  List<bool> answers = [false, true, true];
+//  List<String> questions = [
+//    'You can lead a cow down stairs but not up stairs.',
+//    'Approximately one quarter of human bones are in the feet.',
+//    'A slug\'s blood is green.'
+//  ];
+//  List<bool> answers = [false, true, true];
+//
+
   int questionnumber = 0;
   Widget build(BuildContext context) {
     return Column(
@@ -45,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionnumber % 3],
+                quizz.questiontt(questionnumber),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,7 +77,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked true.
 
                 setState(() {
-                  bool correctanswer = answers[questionnumber % 3];
+                  bool correctanswer = quizz.questionans(questionnumber);
                   if (correctanswer == true) {
                     scorekeeper.add(Icon(
                       Icons.check,
@@ -105,7 +110,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 setState(() {
-                  bool correctanswer = answers[questionnumber % 3];
+                  bool correctanswer = quizz.questionans(questionnumber);
                   if (correctanswer == false) {
                     scorekeeper.add(Icon(
                       Icons.check,
